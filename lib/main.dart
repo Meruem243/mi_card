0b1029a551f9eb5016d30d0736d58fd68621c0c0
meruem243 <mughees.ahmed243@gmail.com>
2023-07-17 04:50:41 +0500

    Testing git 2nd comit added new txt and font files

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                backgroundColor: Colors.lightGreen,
-                radius: 70.0,
-                backgroundImage: AssetImage('images/caol1.jpeg'),
-              ),
-              Text(
-                'Coal sigh',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.lightGreen,
+                  radius: 70.0,
+                  backgroundImage: AssetImage('images/caol1.jpeg'),
                 ),
-              ),
-            ],
+                Text(
+                  'Coal sigh',
+                  style: TextStyle(
+                    fontFamily: 'Lugrasimo',
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Coal Sigh',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    color: Colors.black,
+                    fontSize: 50.0,
+                    // fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
