86c0f6ad348c70939da8faea07a6a4fb75b51f80
meruem243 <mughees.ahmed243@gmail.com>
2023-07-17 23:51:28 +0500

    6-46 Adding Material Icons with the Icon Widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
@@ -17,13 +17,13 @@ class MyApp extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                CircleAvatar(
+                const CircleAvatar(
                   backgroundColor: Colors.lightGreen,
                   radius: 70.0,
                   backgroundImage: AssetImage('images/caol1.jpeg'),
                 ),
-                Text(
-                  'Coal sigh',
+                const Text(
+                  'Name',
                   style: TextStyle(
                     fontFamily: 'Lugrasimo',
                     color: Colors.white,
@@ -31,13 +31,67 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                Text(
-                  'Coal Sigh',
+                const Text(
+                  'Description',
                   style: TextStyle(
-                    fontFamily: 'Roboto',
+                    fontFamily: 'Tangerine',
                     color: Colors.black,
                     fontSize: 50.0,
-                    // fontWeight: FontWeight.bold,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 50.0),
+                  padding: const EdgeInsets.all(10.0),
+                  child: const Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.black38,
+                        size: 30.0,
+                        // semanticLabel: 'Text to announce in accessibility modes',
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        '+123 456 7890',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 50.0),
+                  padding: const EdgeInsets.all(10.0),
+                  child: const Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.black38,
+                        size: 30.0,
+                        // semanticLabel: 'Text to announce in accessibility modes',
+                      ),
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text(
+                        'Example@email.com',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
